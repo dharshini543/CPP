23d0076d1630998026a83645fe82231130b766f0
Dharshini T R <dharshinitr1998@gmail.com>
2025-02-13 09:59:28 +0530

    Fourth Commit

--- C++/Tasks/ArrayTasks/ArraySwap/Task1/ArraySwap1/main.cpp
+++ C++/Tasks/ArrayTasks/ArraySwap/Task1/ArraySwap1/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int size;
+    cout << "Enter size" << endl;
+    cin>>size;
+    int array[size];
+    cout << "Enter Array Elements" << endl;
+    for(int i = 0;i < size;i++)
+    {
+        cin>>array[i];
+    }
+    for(int i = 0;i < size;i++)
+    {
+        if(i%2 != 0 && (i+2)< size)
+        {
+            int temp = array[i];
+            array[i] = array[i + 2];
+            array[i + 2] = temp;
+        }
+    }
+    cout<<endl;
+    for(int i = 0;i < size;i++)
+    {
+        cout<<array[i]<< endl;
+    }
     return 0;
 }
+
+
